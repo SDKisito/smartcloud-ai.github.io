1bf6310017fa42212090affd33c68dc6af707154
SDKisito <saliou.itinova@gmail.com>
2025-04-05 17:40:55 +0200

    Ajout initial des fichiers smartcloud

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Animation pour la navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
+
+// Initialisation des tooltips Bootstrap
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
